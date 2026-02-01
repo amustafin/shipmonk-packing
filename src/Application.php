b7347ba5f0c9faa32c4a7d9e6f6346f6ea000e46
Artem Mustafin <artem.mustafin@netrex.cz>
2026-02-02 00:47:34 +0100

    feat(app): adjust prepared business logic to work on application run call.

--- src/Application.php
+++ src/Application.php
@@ -2,25 +2,32 @@
 
 namespace App;
 
+use App\Facades\PackagingFacade;
+use App\Model\Product\ProductFacade;
 use Doctrine\ORM\EntityManager;
 use GuzzleHttp\Psr7\Response;
 use Psr\Http\Message\RequestInterface;
 use Psr\Http\Message\ResponseInterface;
 
-class Application
+final readonly class Application
 {
+    private PackagingFacade $packagingFacade;
 
-    private EntityManager $entityManager;
+    private ProductFacade $productFacade;
 
     public function __construct(EntityManager $entityManager)
     {
-        $this->entityManager = $entityManager;
+        $this->packagingFacade = PackagingFacade::create($entityManager);
+        $this->productFacade = ProductFacade::create($entityManager);
     }
 
     public function run(RequestInterface $request): ResponseInterface
     {
-        // your implementation entrypoint
-        return new Response();
-    }
+        $productList = $this->productFacade->getOrCreateProductsFromRequest($request->getBody()->getContents());
 
+        return new Response(
+            body: $this->packagingFacade->findBoxForProducts($productList)?->toJson()
+                ?? 'Requested products cannot be packed into any available single box.',
+        );
+    }
 }
