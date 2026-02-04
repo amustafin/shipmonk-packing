fdf60b175468e42ff5d6a0886e48e3ff59fc01da
Artem Mustafin <artem.mustafin@netrex.cz>
2026-02-04 23:33:22 +0100

    feat(internalPackager): implement fallback solution

--- src/Modules/Packaging/InternalPackager/PackagingService.php
+++ src/Modules/Packaging/InternalPackager/PackagingService.php
@@ -7,6 +7,9 @@ namespace App\Modules\Packaging\InternalPackager;
 use App\Model\Box\Box;
 use App\Model\Box\BoxRepository;
 use App\Model\Product\Product;
+use Latuconsinafr\BinPackager\BinPackager3D\Bin;
+use Latuconsinafr\BinPackager\BinPackager3D\Item;
+use Latuconsinafr\BinPackager\BinPackager3D\Packager;
 
 final readonly class PackagingService
 {
@@ -21,7 +24,69 @@ final readonly class PackagingService
     public function findBox(array $products): ?Box
     {
         $availableBoxes = $this->boxRepository->findAll();
+        $bins = $this->prepareBins($availableBoxes);
+        $items = $this->prepareItems($products);
 
-        return $this->boxRepository->find($availableBoxes[0]->getId());
+        foreach ($bins as $key => $bin) {
+            if ($this->isBinSuitable($items, $bin)) {
+                return $this->boxRepository->find($key);
+            }
+        }
+
+        // no suitable box found
+        return null;
+    }
+
+    /**
+     * @param array<Box> $boxes
+     * @return array<Bin>
+     */
+    private function prepareBins(array $boxes): array
+    {
+        $bins = [];
+        foreach ($boxes as $box) {
+            $bins[$box->getId()] = new Bin(
+                id: $box->getId(),
+                length: $box->length,
+                height: $box->height,
+                breadth: $box->width,
+                weight: $box->maxWeight,
+            );
+        }
+        return $bins;
+    }
+
+    /**
+     * @param array<Product> $products
+     * @return array<Item>
+     */
+    private function prepareItems(array $products): array
+    {
+        $items = [];
+        foreach ($products as $product) {
+            $items[] = new Item(
+                id: $product->getId(),
+                length: $product->length,
+                height: $product->height,
+                breadth: $product->width,
+                weight: $product->weight,
+            );
+        }
+        return $items;
+    }
+
+    /**
+     * @param array<Item> $items
+     */
+    private function isBinSuitable(array $items, Bin $bin): bool
+    {
+        $packager = new Packager();
+        $packager->addBin($bin);
+
+        foreach ($items as $item) {
+            $packager->packItemToBin($bin, $item);
+        }
+
+        return $bin->getTotalUnfittedVolume() === 0.0;
     }
 }
--- src/Modules/Packaging/PackagingFacade.php
+++ src/Modules/Packaging/PackagingFacade.php
@@ -27,15 +27,19 @@ final readonly class PackagingFacade
     {
         $productIds = array_map(fn (Product $product) => $product->getId(), $products);
         $totalWeight = array_reduce($products, fn (float $carry, Product $product) => $carry + $product->weight, 0);
+
+        $box = $this->orderRepository->findByProducts(implode(',', $productIds))?->box;
+        if ($box !== null) {
+            return $box;
+        }
+
         $box =
-            // Try to find a known configuration in db
-            $this->orderRepository->findByProducts(implode(',', $productIds))->box
             // try to find box from api
-            ?? $this->remotePackagingService->findBox($products)
+            $this->remotePackagingService->findBox($products)
             // fallback to any box that can hold the weight
             ?? $this->internalPackagingService->findBox($products);
 
-        // if now box is null, products cannot be packed into any available single box
+        $box = $box->maxWeight ?? 0 >= $totalWeight ? $box : null;
 
         $productIds = array_map(fn (Product $product) => $product->getId(), $products);
         $order = new Order(
@@ -44,6 +48,6 @@ final readonly class PackagingFacade
         );
         $this->orderRepository->save($order);
 
-        return $box->maxWeight ?? 0 >= $totalWeight ? $box : null;
+        return $order->box;
     }
 }
